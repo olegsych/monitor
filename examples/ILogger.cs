307e18cd9175d028bcf8717691d3486ca6f74c5a
Oleg Sych <olegsych@users.noreply.github.com>
2021-05-01 18:42:29 -0700

    Make ILogger example use Arg.Is instead of Arg.Any for formatter

--- examples/ILogger.cs
+++ examples/ILogger.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq.Expressions;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
 using Xunit;
@@ -14,6 +15,7 @@ namespace Monitor
         readonly ILogger<Subject> logger = Substitute.For<ILogger<Subject>>();
 
         readonly Exception? noException = null;
+        readonly Expression<Predicate<Func<Subject, Exception, string>>> expectedFormatter = _ => true;
 
         public class Observe: ILogger
         {
@@ -23,7 +25,7 @@ namespace Monitor
                 monitor.Observe(subject);
 
                 // What happens
-                logger.Received().Log(LogLevel.Information, Arg.Any<EventId>(), subject, noException, Arg.Any<Func<Subject, Exception, string>>());
+                logger.Received().Log(LogLevel.Information, Arg.Any<EventId>(), subject, noException, Arg.Is(expectedFormatter));
             }
 
             [Fact]
@@ -38,7 +40,7 @@ namespace Monitor
                 }
 
                 // What happens
-                logger.Received().Log(LogLevel.Error, Arg.Any<EventId>(), subject, exception, Arg.Any<Func<Subject, Exception, string>>());
+                logger.Received().Log(LogLevel.Error, Arg.Any<EventId>(), subject, exception, Arg.Is(expectedFormatter));
             }
         }
 
@@ -52,7 +54,7 @@ namespace Monitor
 
                 // What happens
                 object assert = logger.Received().BeginScope(subject);
-                logger.Received().Log(LogLevel.Information, Arg.Any<EventId>(), subject, noException, Arg.Any<Func<Subject, Exception, string>>());
+                logger.Received().Log(LogLevel.Information, Arg.Any<EventId>(), subject, noException, Arg.Is(expectedFormatter));
             }
 
             [Fact]
@@ -68,7 +70,7 @@ namespace Monitor
                 }
 
                 // What happens
-                logger.Received().Log(LogLevel.Error, Arg.Any<EventId>(), subject, noException, Arg.Any<Func<Subject, Exception, string>>());
+                logger.Received().Log(LogLevel.Error, Arg.Any<EventId>(), subject, noException, Arg.Is(expectedFormatter));
             }
         }
     }
