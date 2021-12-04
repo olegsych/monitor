92d557aeb4bdf9809c03427d774a95435678c86a
Oleg Sych <olegsych@users.noreply.github.com>
2021-12-04 12:18:13 -0800

    Cleanup naming in IMonitorExtensions

--- src/IMonitorExtensions.cs
+++ src/IMonitorExtensions.cs
@@ -8,76 +8,76 @@ namespace Monitor
     {
         #region Instrument
 
-        public static IInstrument Instrument(this IMonitor factory, Action action) =>
-            factory.Instrument(action.Method);
+        public static IInstrument Instrument(this IMonitor monitor, Action method) =>
+            monitor.Instrument(method.Method);
 
-        public static IInstrument Instrument(this IMonitor factory, Func<Task> command) =>
+        public static IInstrument Instrument(this IMonitor monitor, Func<Task> method) =>
             throw new NotImplementedException();
 
-        public static IInstrument Instrument(this IMonitor factory, Func<ValueTask> command) =>
+        public static IInstrument Instrument(this IMonitor monitor, Func<ValueTask> method) =>
             throw new NotImplementedException();
 
-        public static IInstrument Instrument(this IMonitor factory, Func<CancellationToken, Task> command) =>
+        public static IInstrument Instrument(this IMonitor monitor, Func<CancellationToken, Task> method) =>
             throw new NotImplementedException();
 
-        public static IInstrument Instrument(this IMonitor factory, Func<CancellationToken, ValueTask> command) =>
+        public static IInstrument Instrument(this IMonitor monitor, Func<CancellationToken, ValueTask> method) =>
             throw new NotImplementedException();
 
         #endregion
 
         #region Instrument<TInput>
 
-        public static IInstrument<T> Instrument<T>(this IMonitor factory, Action<T> command) =>
+        public static IInstrument<T> Instrument<T>(this IMonitor monitor, Action<T> method) =>
             throw new NotImplementedException();
 
-        public static IInstrument<T> Instrument<T>(this IMonitor factory, Func<T, Task> command) =>
+        public static IInstrument<T> Instrument<T>(this IMonitor monitor, Func<T, Task> method) =>
             throw new NotImplementedException();
 
-        public static IInstrument<T> Instrument<T>(this IMonitor factory, Func<T, CancellationToken, Task> command) =>
+        public static IInstrument<T> Instrument<T>(this IMonitor monitor, Func<T, CancellationToken, Task> method) =>
             throw new NotImplementedException();
 
-        public static IInstrument<T> Instrument<T>(this IMonitor factory, Func<T, ValueTask> command) =>
+        public static IInstrument<T> Instrument<T>(this IMonitor monitor, Func<T, ValueTask> method) =>
             throw new NotImplementedException();
 
-        public static IInstrument<T> Instrument<T>(this IMonitor factory, Func<T, CancellationToken, ValueTask> command) =>
+        public static IInstrument<T> Instrument<T>(this IMonitor monitor, Func<T, CancellationToken, ValueTask> method) =>
             throw new NotImplementedException();
 
         #endregion
 
         #region Instrument<TOutput>
 
-        public static IInstrument<T> Instrument<T>(this IMonitor factory, Func<T> command) =>
+        public static IInstrument<T> Instrument<T>(this IMonitor monitor, Func<T> method) =>
             throw new NotImplementedException();
 
-        public static IInstrument<T> Instrument<T>(this IMonitor factory, Func<Task<T>> command) =>
+        public static IInstrument<T> Instrument<T>(this IMonitor monitor, Func<Task<T>> method) =>
             throw new NotImplementedException();
 
-        public static IInstrument<T> Instrument<T>(this IMonitor factory, Func<CancellationToken, Task<T>> command) =>
+        public static IInstrument<T> Instrument<T>(this IMonitor monitor, Func<CancellationToken, Task<T>> method) =>
             throw new NotImplementedException();
 
-        public static IInstrument<T> Instrument<T>(this IMonitor factory, Func<ValueTask<T>> command) =>
+        public static IInstrument<T> Instrument<T>(this IMonitor monitor, Func<ValueTask<T>> method) =>
             throw new NotImplementedException();
 
-        public static IInstrument<T> Instrument<T>(this IMonitor factory, Func<CancellationToken, ValueTask<T>> command) =>
+        public static IInstrument<T> Instrument<T>(this IMonitor monitor, Func<CancellationToken, ValueTask<T>> method) =>
             throw new NotImplementedException();
 
         #endregion
 
         #region Instrument<TInput, TOutput>
 
-        public static IInstrument<TInput, TOutput> Instrument<TInput, TOutput>(this IMonitor factory, Func<TInput, TOutput> query) =>
+        public static IInstrument<TInput, TOutput> Instrument<TInput, TOutput>(this IMonitor monitor, Func<TInput, TOutput> method) =>
             throw new NotImplementedException();
 
-        public static IInstrument<TInput, TOutput> Instrument<TInput, TOutput>(this IMonitor factory, Func<TInput, Task<TOutput>> query) =>
+        public static IInstrument<TInput, TOutput> Instrument<TInput, TOutput>(this IMonitor monitor, Func<TInput, Task<TOutput>> method) =>
             throw new NotImplementedException();
 
-        public static IInstrument<TInput, TOutput> Instrument<TInput, TOutput>(this IMonitor factory, Func<TInput, CancellationToken, Task<TOutput>> query) =>
+        public static IInstrument<TInput, TOutput> Instrument<TInput, TOutput>(this IMonitor monitor, Func<TInput, CancellationToken, Task<TOutput>> method) =>
             throw new NotImplementedException();
 
-        public static IInstrument<TInput, TOutput> Instrument<TInput, TOutput>(this IMonitor factory, Func<TInput, ValueTask<TOutput>> query) =>
+        public static IInstrument<TInput, TOutput> Instrument<TInput, TOutput>(this IMonitor monitor, Func<TInput, ValueTask<TOutput>> method) =>
             throw new NotImplementedException();
 
-        public static IInstrument<TInput, TOutput> Instrument<TInput, TOutput>(this IMonitor factory, Func<TInput, CancellationToken, ValueTask<TOutput>> query) =>
+        public static IInstrument<TInput, TOutput> Instrument<TInput, TOutput>(this IMonitor monitor, Func<TInput, CancellationToken, ValueTask<TOutput>> method) =>
             throw new NotImplementedException();
 
         #endregion
