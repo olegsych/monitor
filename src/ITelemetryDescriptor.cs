5c837f5026167f38619a080d5aa510d1c5e8de55
Oleg Sych <olegsych@users.noreply.github.com>
2021-12-04 16:33:23 -0800

    Comment ITelemetryDescriptor<>

--- src/ITelemetryDescriptor.cs
+++ src/ITelemetryDescriptor.cs
@@ -3,9 +3,16 @@ namespace Monitor
     /// <summary>
     /// Describes telemetry of a given <typeparamref name="TSubject"/> type.
     /// </summary>
-    /// <typeparam name="TSubject"><
+    /// <typeparam name="TSubject">
     /// Type for which telemetry needs to be collected.
     /// </typeparam>
+    /// <remarks>
+    /// <see cref="Monitor"/> will provide a default implementation of
+    /// <see cref="ITelemetryDescriptor{TSubject}"/> that infers telemetry from the
+    /// public <typeparamref name="TSubject"/> properties and fields using reflection.
+    /// Application developers can provide their own implementation of this interface
+    /// to take complete control of telemetry extraction of their types.
+    /// </remarks>
     public interface ITelemetryDescriptor<TSubject>
     {
         void Describe(ITelemetryDescription<TSubject> description);
