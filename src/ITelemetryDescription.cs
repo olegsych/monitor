a5cce635a171b9cb5843b961bc72d7df0c436f05
Oleg Sych <olegsych@users.noreply.github.com>
2021-12-04 16:40:44 -0800

    Comment ITelemetryDescription<>

--- src/ITelemetryDescription.cs
+++ src/ITelemetryDescription.cs
@@ -3,6 +3,12 @@ using System.Linq.Expressions;
 
 namespace Monitor
 {
+    /// <summary>
+    /// Collects descriptinos of the <typeparamref name="TSubject"/> telemetry.
+    /// </summary>
+    /// <typeparam name="TSubject">
+    /// Type whose telemetry is being described.
+    /// </typeparam>
     public interface ITelemetryDescription<TSubject>
     {
         void AddProperty<TValue>(Expression<Func<TSubject, TValue>> getter);
