d3a76a9a926565f6ea7409517deb296607e0ec41
Oleg Sych <olegsych@users.noreply.github.com>
2021-12-04 18:39:26 -0800

    Comment ITelemetryDescription

--- src/ITelemetryDescription.cs
+++ src/ITelemetryDescription.cs
@@ -11,8 +11,22 @@ namespace Monitor
     /// </typeparam>
     public interface ITelemetryDescription<TSubject>
     {
+        /// <summary>
+        /// Adds a property to telemetry of <typeparamref name="TSubject"/>.
+        /// Properties can be of any type convertible to string for logging.
+        /// </summary>
         void AddProperty<TValue>(Expression<Func<TSubject, TValue>> getter);
+
+        /// <summary>
+        /// Adds a metric to telemetry of <typeparamref name="TSubject"/>.
+        /// Metrics must be convertible to integer for aggregation.
+        /// </summary>
         void AddMetric<TValue>(Expression<Func<TSubject, TValue>> getter);
+
+        /// <summary>
+        /// Adds a dimension to telemetry of <typeparamref name="TSubject"/>.
+        /// Dimensions are low-cardinality properties used for metric aggregation.
+        /// </summary>
         void AddDimension<TValue>(Expression<Func<TSubject, TValue>> getter);
     }
 }
