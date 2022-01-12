d74aae4746dc6586fb290d431d4d3387365fb13a
Oleg Sych <olegsych@users.noreply.github.com>
2022-01-12 13:34:49 -0800

    Initial incomplete version of Instrument example

---
+++ examples/InstrumentExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Monitor
+{
+    class InstrumentExample
+    {
+        class Subject
+        {
+            public int Foo;
+            public string? Bar;
+            public Color Color;
+        }
+
+        enum Color
+        {
+            Red, Green, Blue
+        }
+
+        class SubjectInstrument: IInstrument<Subject>
+        {
+            public void Record(Measurement measurement = default, Exception? exception = null, Subject? subject = null) => throw new NotImplementedException();
+            public Measurement Start() => throw new NotImplementedException();
+        }
+
+    }
+}
