ae88c63fc36812da98b76038d3284fd9e6c810c9
Oleg Sych <olegsych@users.noreply.github.com>
2022-01-12 16:17:23 -0800

    Fix Monitor compiler warnings

--- src/Monitor/IInstrumentExtensions.cs
+++ src/Monitor/IInstrumentExtensions.cs
@@ -5,27 +5,27 @@ namespace Monitor
     public static class IInstrumentExtensions
     {
         public static void Record(this IInstrument instrument, Exception exception) =>
-            instrument.Record(default, exception);
+            throw new NotImplementedException();
 
         public static void Record<T>(this IInstrument<T> instrument, T subject) =>
-            instrument.Record(default, default, subject);
+            throw new NotImplementedException();
 
         public static void Record<T>(this IInstrument<T> instrument, Exception exception) =>
-            instrument.Record(default, exception, default);
+            throw new NotImplementedException();
 
         public static void Record<T>(this IInstrument<T> instrument, Measurement measurement, T subject) =>
-            instrument.Record(measurement, default, subject);
+            throw new NotImplementedException();
 
         public static void Record<T>(this IInstrument<T> instrument, Exception exception, T subject) =>
-            instrument.Record(default, exception, subject);
+            throw new NotImplementedException();
 
         public static void Record<T1, T2>(this IInstrument<T1, T2> instrument, T1 subject1, T2 subject2) =>
-            instrument.Record(default, default, subject1, subject2);
+            throw new NotImplementedException();
 
         public static void Record<T1, T2>(this IInstrument<T1, T2> instrument, Exception exception, T1 subject1) =>
-            instrument.Record(default, exception, subject1, default);
+            throw new NotImplementedException();
 
         public static void Record<T1, T2>(this IInstrument<T1, T2> instrument, Measurement measurement, T1 subject1, T2 subject2) =>
-            instrument.Record(measurement, default, subject1, subject2);
+            throw new NotImplementedException();
     }
 }
